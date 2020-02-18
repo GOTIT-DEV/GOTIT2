62222379f6e30b2e9316c96eb6bbd965fb14ddef
Louis Duchemin <ls.duchemin@gmail.com>
2020-02-18 12:00:33 +0100

    [QBuilder] Fixed deprecated routing annotations

--- src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
+++ src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
@@ -2,8 +2,8 @@
 
 namespace Lehna\QueryBuilderBundle\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 /**
