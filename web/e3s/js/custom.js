374a8837336afe0d6388bb6c16082d03bc29687f
Louis Duchemin <ls.duchemin@gmail.com>
2020-02-20 12:35:09 +0100

    bkp

--- web/e3s/js/custom.js
+++ web/e3s/js/custom.js
@@ -69,70 +69,68 @@ var setContentHeight = function () {
 	$RIGHT_COL.css('min-height', contentHeight);
 };
 
-  $SIDEBAR_MENU.find('a').on('click', function(ev) {
-	  console.log('clicked - sidebar_menu');
-        var $li = $(this).parent();
+	// $SIDEBAR_MENU.find('a').on('click', function (ev) {
+	// 	console.log('clicked - sidebar_menu');
+	// 	var $li = $(this).parent();
 
-        if ($li.is('.active')) {
-            $li.removeClass('active active-sm');
-            $('ul:first', $li).slideUp(function() {
-                setContentHeight();
-            });
-        } else {
-            // prevent closing menu if we are on child menu
-            if (!$li.parent().is('.child_menu')) {
-                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                $SIDEBAR_MENU.find('li ul').slideUp();
-            }else
-            {
-				if ( $BODY.is( ".nav-sm" ) )
-				{
-					$SIDEBAR_MENU.find( "li" ).removeClass( "active active-sm" );
-					$SIDEBAR_MENU.find( "li ul" ).slideUp();
-				}
-			}
-            $li.addClass('active');
+	// 	if ($li.is('.active')) {
+	// 		$li.removeClass('active active-sm');
+	// 		$('ul:first', $li).slideUp(function () {
+	// 			setContentHeight();
+	// 		});
+	// 	} else {
+	// 		// prevent closing menu if we are on child menu
+	// 		if (!$li.parent().is('.child_menu')) {
+	// 			$SIDEBAR_MENU.find('li').removeClass('active active-sm');
+	// 			$SIDEBAR_MENU.find('li ul').slideUp();
+	// 		} else {
+	// 			if ($BODY.is(".nav-sm")) {
+	// 				$SIDEBAR_MENU.find("li").removeClass("active active-sm");
+	// 				$SIDEBAR_MENU.find("li ul").slideUp();
+	// 			}
+	// 		}
+	// 		$li.addClass('active');
 
-            $('ul:first', $li).slideDown(function() {
-                setContentHeight();
-            });
-        }
-    });
+	// 		$('ul:first', $li).slideDown(function () {
+	// 			setContentHeight();
+	// 		});
+	// 	}
+	// });
 
 // toggle small or large menu 
-$MENU_TOGGLE.on('click', function() {
-		console.log('clicked - menu toggle');
+// $MENU_TOGGLE.on('click', function() {
+// 		console.log('clicked - menu toggle');
 		
-		if ($BODY.hasClass('nav-md')) {
-			$SIDEBAR_MENU.find('li.active ul').hide();
-			$SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
-		} else {
-			$SIDEBAR_MENU.find('li.active-sm ul').show();
-			$SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
-		}
+// 		if ($BODY.hasClass('nav-md')) {
+// 			$SIDEBAR_MENU.find('li.active ul').hide();
+// 			$SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+// 		} else {
+// 			$SIDEBAR_MENU.find('li.active-sm ul').show();
+// 			$SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+// 		}
 
-	$BODY.toggleClass('nav-md nav-sm');
+// 	$BODY.toggleClass('nav-md nav-sm');
 
-	setContentHeight();
+// 	setContentHeight();
 
-	$('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
-});
+// 	$('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
+// });
 
-	// check active menu
-	$SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+	// // check active menu
+	// $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-	$SIDEBAR_MENU.find('a').filter(function () {
-		return this.href == CURRENT_URL;
-	}).parent('li').addClass('current-page').parents('ul').slideDown(function() {
-		setContentHeight();
-	}).parent().addClass('active');
+	// $SIDEBAR_MENU.find('a').filter(function () {
+	// 	return this.href == CURRENT_URL;
+	// }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
+	// 	setContentHeight();
+	// }).parent().addClass('active');
 
-	// recompute content when resizing
-	$(window).smartresize(function(){  
-		setContentHeight();
-	});
+	// // recompute content when resizing
+	// $(window).smartresize(function(){  
+	// 	setContentHeight();
+	// });
 
-	setContentHeight();
+	// setContentHeight();
 
 	// fixed sidebar
 	if ($.fn.mCustomScrollbar) {
