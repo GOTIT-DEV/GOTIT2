a1d88c84529c6b15500256f7e5aa31bbce61fb54
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2020-02-21 18:50:43 +0100

    Reponsive left nav

--- web/e3s/js/nav.js
+++ web/e3s/js/nav.js
@@ -1,27 +1,33 @@
-$(".nav-left .menu-entry.active ul:first").slideDown(0)
+function close_sub_menus() {
+  $(".nav-left .menu-entry.active")
+    .removeClass("active")
+    .find('ul:first')
+    .slideUp("fast")
+}
 
-$(".nav-left .menu-entry>a").click(ev => {
+$(".nav-left .menu-entry>a").click(event => {
   let target = $(event.currentTarget)
-
-  if(target.attr("href") === "#"){
+  let entry_elt = target.parent('li.menu-entry');
+  
+  if (target.attr("href") === "#") {
     event.preventDefault()
   }
-  let entry_elt = target.parent('li.menu-entry');
 
   if (entry_elt.hasClass('active')) {
     entry_elt.removeClass('active active-sm')
     $('ul:first', entry_elt).slideUp()
   } else {
-    $(".nav-left .menu-entry.active")
-      .removeClass("active")
-      .find('ul:first')
-      .slideUp("fast")
+    close_sub_menus()
     entry_elt.addClass("active")
     $('ul:first', entry_elt).slideDown("fast")
   }
 })
 
 
-$('#menu_toggle').click(ev => {
+$('#menu-toggle').click(event => {
   $('.nav-left').toggleClass("nav-sm")
+  if ($('.nav-left').hasClass("nav-sm"))
+    Cookies.set("gotit-menu-layout", "small")
+  else
+    Cookies.set("gotit-menu-layout", undefined)
 })
