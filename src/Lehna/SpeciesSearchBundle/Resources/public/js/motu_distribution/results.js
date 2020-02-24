b82f88588c8fdb9513438fb7eb2acaefa113ae23
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2020-02-24 12:12:55 +0100

    [SpSearch] Fixed missing taxname in result tables

--- src/Lehna/SpeciesSearchBundle/Resources/public/js/motu_distribution/results.js
+++ src/Lehna/SpeciesSearchBundle/Resources/public/js/motu_distribution/results.js
@@ -21,7 +21,7 @@ const form = $("#main-form")
 const renderNumber = $.fn.dataTable.render.number('', '.', 3)
 const columns = [
   dtconfig.expandColumn, {
-    data: "taxname",
+    data: "taxon_name",
     render: linkify("referentieltaxon_show", { col: 'taxon_id' })
   }, {
     data: 'code',
