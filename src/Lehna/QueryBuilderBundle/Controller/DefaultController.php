64bd01889a01b4301c19e67f6e4a2648b303da1e
Louis Duchemin <ls.duchemin@gmail.com>
2020-02-24 20:09:46 +0100

    Parser config as service

--- src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
+++ src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
@@ -2,15 +2,13 @@
 
 namespace Lehna\QueryBuilderBundle\Controller;
 
-use Bbees\E3sBundle\BbeesE3sBundle;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use FL\QBJSParserBundle\Service\JavascriptBuilders;
 use Symfony\Component\HttpFoundation\Request;
 use Bbees\E3sBundle\Entity;
-use Doctrine\Persistence\Mapping\ClassMetadata;
+use Lehna\QueryBuilderBundle\Services\QueryBuilderService;
 
 /**
  * Controller for querying COI sampling coverage
@@ -26,58 +24,18 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $x = $this->getDoctrine()->getConnection()->getSchemaManager();
-        $x = $this->getDoctrine()->getManager();
-        // $bundle = $this->getKernel()->getBundle("BbeesE3sBundle");
-        // $meta = $x->getMetadataFactory()->getBundleMetadata($bundle);
-        $meta = $x->getMetadataFactory()->getAllMetadata();
-
-        // dump($meta[0]);
-        // dump($meta[0]->fieldMappings)
-
-        return $this->render('@LehnaQueryBuilder/index.html.twig', [
-            "meta" => $meta[0]#->getAssociationMappings()
-        ]);
+        return $this->render('@LehnaQueryBuilder/index.html.twig');
     }
 
     /**
      * @Route("/init", name="querybuilder_init", methods={"GET"})
      * 
      */
-    public function init_builders()
+    public function init_builders(QueryBuilderService $service)
     {
-        $x = $this->getDoctrine()->getConnection()->getSchemaManager();
-        $x = $this->getDoctrine()->getManager();
-        // $bundle = $this->getKernel()->getBundle("BbeesE3sBundle");
-        // $meta = $x->getMetadataFactory()->getBundleMetadata($bundle);
-        $meta = $x->getMetadataFactory()->getAllMetadata();
-
-        // dump($meta[0]);
-        // dump($meta[0]->fieldMappings);#getFieldMapping("codeCommune"));
-
-        $res = $this->parse_metadata($meta[0]);
-        return new JsonResponse($res);
+        $config = $service->make_qbuilder_config();
+        return new JsonResponse($config);
     }
-    private function parse_metadata(ClassMetadata $metadata)
-    {
-        $make_filter = function ($field) {
-            return [
-                "id" => $field['fieldName'],
-                "label" => $field['fieldName'],
-                "type" => $field['type']
-            ];
-        };
-        $entity = $metadata->name;
-        $filters = array_map($make_filter, $metadata->fieldMappings);
-        $relations = 
-        return [
-            "class" => $entity,
-            "filters" => $filters,
-            "human_readable_name" => $entity,
-            "table" => $metadata->table["name"]
-        ];
-    }
-
 
     /**
      * @Route("/parser-test", name="parser-test", methods={"POST"})
---
+++ src/Lehna/QueryBuilderBundle/Services/QueryBuilderService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Lehna\QueryBuilderBundle\Services;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\Mapping\ClassMetadata;
+use Doctrine\Bundle\DoctrineBundle\Mapping\DisconnectedMetadataFactory;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Service QueryBuilderService
+ */
+class QueryBuilderService
+{
+  private $doctrineManagerRegistry;
+  private $metadataManager;
+  private $container;
+
+  public function __construct(ManagerRegistry $manager, ContainerInterface $container)
+  {
+    $this->container = $container;
+    $this->doctrineManagerRegistry = $manager;
+    $this->metadataManager =
+      new DisconnectedMetadataFactory($this->doctrineManagerRegistry);
+  }
+
+  public function make_qbuilder_config()
+  {
+    $coreBundle = $this->container->get("kernel")->getBundle("BbeesE3sBundle");
+    $metadata = $this->metadataManager
+      ->getBundleMetadata($coreBundle)
+      ->getMetadata();
+
+    return $this->parse_entities_metadata($metadata);
+  }
+
+  private function parse_entity_name($entity)
+  {
+    $entity = explode('\\', $entity);
+    return array_pop($entity);
+  }
+
+  public function parse_entities_metadata($metadata_array)
+  {
+    $res = [];
+    $relations = [];
+    foreach ($metadata_array as $m) {
+      $entity = $this->parse_entity_name($m->getName());
+      $res[$entity] = $this->parse_metadata($m);
+      $relations[$entity] = [];
+      foreach ($m->getAssociationMappings() as $field => $mapping) {
+        if (array_key_exists("joinColumns", $mapping)) {
+          $target = $this->parse_entity_name($mapping['targetEntity']);
+          $relations[$entity][$target] = $this->parse_associated($mapping);
+        }
+      }
+    }
+    foreach ($relations as $sourceEntity => $targets) {
+      foreach ($targets as $targetEntity => $data) {
+        $relations[$targetEntity][$sourceEntity] = [
+          "entity" => $sourceEntity,
+          "from" => $data["to"],
+          "to" => $data["from"]
+        ];
+      }
+    }
+    foreach ($res as $entity => $data) {
+      $res[$entity]["relations"] = $relations[$entity];
+    }
+    return $res;
+  }
+
+  private function parse_associated($mapping)
+  {
+    return [
+      "entity" => $this->parse_entity_name($mapping["targetEntity"]),
+      "from" => $mapping["joinColumns"][0]["name"],
+      "to" => $mapping["joinColumns"][0]["referencedColumnName"]
+    ];
+  }
+
+  private function parse_metadata(ClassMetadata $metadata)
+  {
+    $make_filter = function ($field) {
+      return [
+        "id" => $field['fieldName'],
+        "label" => $field['fieldName'],
+        "type" => $field['type']
+      ];
+    };
+    $entity = $metadata->name;
+    $filters = array_map($make_filter, $metadata->fieldMappings);
+    return [
+      "class" => $entity,
+      "filters" => $filters,
+      "human_readable_name" => $this->parse_entity_name($entity),
+      "table" => $metadata->table["name"]
+    ];
+  }
+}
