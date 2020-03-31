3bfea5d8f2c6187bf1e9d56f8d28d12c08aeff4e
Maud <maud.ferrer9@orange.fr>
2020-03-31 17:33:01 +0200

    Adding the possibility to make a query with one level of condition ('AND' or 'OR')for the rules, still can't add group

--- src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
+++ src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use Lehna\QueryBuilderBundle\Services\BuilderQueryService;
 use Lehna\SpeciesSearchBundle\Services\SpeciesQueryService;
 use Bbees\E3sBundle\Services\GenericFunctionService;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 use Bbees\E3sBundle\Entity\ACibler;
 use Bbees\E3sBundle\Entity\Adn;
@@ -70,15 +71,14 @@ class DefaultController extends Controller
      *  @Route("/query", name="query_test", methods={"POST"})
      * 
      */
-    public function get_query(Request $request)
-    {
+    public function getRequestBuilder(Request $request) {
+        
         $data = $request->request->all();
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
         $initial = $data["initial"];
         $query = $this->getFirstBlock($initial, $qb);
-        //$query = $this->getFirstTable($data, $query);
-        //$query = $this->getFirstConstraints($data, $query);
+        dump($query);
         $q = $query->getQuery();
         dump($q);
         $results = $q->getArrayResult();
@@ -87,107 +87,6 @@ class DefaultController extends Controller
         
     }
 
-    /**
-     * Lists all user entities. 
-     * @Route("/ab", name="user_index", methods={"GET"})
-     * 
-     */
-    public function showUser(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $users = $em->getRepository('BbeesUserBundle:User')->findAll();
-        dump($users);
-        return $this->render('@LehnaQueryBuilder/results.html.twig',['users'=>$users]);
-    }
-
-
-    /**
-     * Lists all station entities. 
-     * @Route("/sanscontraintes", name="tablesSans_get", methods={"GET"})
-     * 
-     */
-    public function showStation2(Request $request)
-    {
-        $service = $this->get('bbees_e3s.generic_function_e3s');
-        $em = $this->getDoctrine()->getManager();
-        $tables = $em->getRepository('BbeesE3sBundle:Station')->createQueryBuilder('station')
-            ->leftJoin('BbeesE3sBundle:Pays', 'pays', 'WITH', 'station.paysFk = pays.id')
-            ->getQuery()
-            ->getResult();
-        $tab_toshow =[];
-        foreach($tables as $entity)
-        {
-            $tab_toshow[] = array( "station.id" => $entity->getId(), "station.codeStation" => $entity->getCodeStation(),
-             "station.nomStation" => $entity->getNomStation(),
-             "pays.codePays" => $entity->getPaysFk()->getCodePays());
-        }
-        $response = new Response ();
-        $response->setContent ( json_encode ( array ( 
-            "rows"  => $tab_toshow       			
-            ) ) );
-        // If it is an Ajax request: returns the content in json format
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;  
-    }
-
-    /**
-    * Lists all station entities. 
-    * @Route("/getgenusset", name="get_json", methods={"GET"})
-    * 
-    */
-    
-
-    public function getGenusSet()
-    {
-        $qb    = $this->entityManager->createQueryBuilder();
-        $query = $qb->select('rt.genus')
-        ->from('BbeesE3sBundle:ReferentielTaxon', 'rt')
-        ->where('rt.genus IS NOT NULL')
-        ->distinct()
-        ->orderBy('rt.genus')
-        ->getQuery();
-        return $query->getResult();
-    }
-
-    /**
-    * Lists all station entities. 
-    * @Route("/getgenusset2", name="get_json2", methods={"GET"})
-    * 
-    */
-
-    public function getGenusSet2(Request $request)
-    {
-        $request->request->get("field");
-        $qb    = $this->entityManager->createQueryBuilder();
-        $query = $qb->from('BbeesE3sBundle:ReferentielTaxon', 'rt');
-        // on tombe sur une contrainte
-        $query = $query->where('rt.genus IS NOT NULL');
-        // etc...
-        $query = $query->select("rt.genus");
-
-        $query->distinct()
-        ->orderBy('rt.genus')
-        ->getQuery();
-        return $query->getResult();
-    }
-
-    /**
-    * Lists all station entities. 
-    * @Route("/searchquery", name="search_query", methods={"GET"})
-    * 
-    */
-    public function searchQuery(Request $request, SpeciesQueryService $service) {
-        # POST parameters
-        $data = $request->request;
-        # execute query
-        $res  = $service->getMotuCountList($data);
-        # return JSON response
-        return new JsonResponse(array(
-        'rows'     => $res,
-        'query'    => $data->all(),
-        ));
-    }
 
     /**
     * Get the first fields of the first table that we want to return and creates the "select" part of the query. 
@@ -195,83 +94,151 @@ class DefaultController extends Controller
     * 
     */
     public function getFirstBlock($initial, $query) {
+        
         $firstTable = $initial["table"];
         $query = $query->from('BbeesE3sBundle:'.$firstTable, $firstTable);
         $firstFields = $initial["fields"];
+        $condition = $initial["constraintsTable1"]["condition"];
+        $firstConstraints = $initial["constraintsTable1"]["rules"];
+        
         foreach ($firstFields as $value){
             $query = $query->addSelect($firstTable.".".$value);
         };
-        $query = $this->getFirstConstraints($initial, $query, $firstTable);
 
+        if (strlen($initial["constraintsTable1"]["rules"] == 1)) {
+            $query = $this->getFirstConstraints($firstConstraints, $initial, $query, $firstTable, $condition);
+        } elseif (strlen($initial["constraintsTable1"]["rules"] > 1)) {
+            foreach ($initial["constraintsTable1"]["rules"] as $r) {
+                dump(count($r));
+                $query = $this->getFirstConstraints($r, $initial, $query, $firstTable, $condition);
+                
+            }
+        }
         return $query;
     }
-
-
-    /**
-    * Get the first table. 
-    * @Route("/query", name="test_query", methods={"POST"})
-    * 
-    */
-    /*
-    public function getFirstTable($data, $query) {
-        $firstTable = $data["initial"]["table"];
-        $query = $query->from('BbeesE3sBundle:'.$firstTable, $firstTable);
-        return $query;
-    }
-*/
 
     /**
     * Get the first constraints. 
     * @Route("/query", name="test_query", methods={"POST"})
     * 
     */
-    
-    public function getFirstConstraints($initial, $query, $firstTable) {
-        $firstConstraints = $initial["constraintsTable1"]["rules"][0];
+    public function getFirstConstraints($firstConstraints, $initial, $query, $firstTable, $condition) {
         $firstField = $firstConstraints["field"];
         $firstOperator = $firstConstraints["operator"];
         $firstValue = $firstConstraints["value"];
         $ft = $firstTable.".".$firstField;
         if ($firstOperator == "equal") {
-            $query = $query->where($ft." = ".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." = ".$firstValue);
+            } else {
+                $query = $query->andWhere($ft." = ".$firstValue);
+            }
         } elseif ($firstOperator == "not_equal"){
-            $query = $query->where($ft."!=".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft."!=".$firstValue);
+            } else {
+                $query = $query->andWhere($ft."!=".$firstValue);
+            }
         } elseif ($firstOperator == "less") {
-            $query = $query->where($ft."<".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft."<".$firstValue);
+            } else {
+                $query = $query->andWhere($ft."<".$firstValue);
+            }
         } elseif ($firstOperator == "less_or_equal") {
-            $query = $query->where($ft."<=".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft."<=".$firstValue);
+            } else {
+                $query = $query->andWhere($ft."<=".$firstValue);
+            }
         } elseif ($firstOperator == "greater") {
-            $query = $query->where($ft.">".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft.">".$firstValue);
+            } else {
+                $query = $query->andWhere($ft.">".$firstValue);
+            }
         } elseif ($firstOperator == "greater_or_equal") {
-            $query = $query->where($ft.">=".$firstValue);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft.">=".$firstValue);
+            } else {
+                $query = $query->andWhere($ft.">=".$firstValue);
+            }
         } elseif ($firstOperator == "begins_with") {
-            $query = $query->where($ft." LIKE"." '".strtoupper($firstValue)."%'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." LIKE"." '".strtoupper($firstValue)."%'");
+            } else {
+                $query = $query->andWhere($ft." LIKE"." '".strtoupper($firstValue)."%'");
+            }
         } elseif ($firstOperator == "not_begins_with") {
-            $query = $query->where($ft." NOT LIKE"." '".strtoupper($firstValue)."%'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." NOT LIKE"." '".strtoupper($firstValue)."%'");
+            } else {
+                $query = $query->andWhere($ft." NOT LIKE"." '".strtoupper($firstValue)."%'");
+            }
         } elseif ($firstOperator == "is_null") {
-            $query = $query->where($ft." ".$firstValue." IS NULL");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." ".$firstValue." IS NULL");
+            } else {
+                $query = $query->andWhere($ft." ".$firstValue." IS NULL");
+            }
         } elseif ($firstOperator == "is_not_null") {
-            $query = $query->where($ft." ".$firstValue." IS NOT NULL");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." ".$firstValue." IS NOT NULL");
+            } else {
+                $query = $query->andWhere($ft." ".$firstValue." IS NOT NULL");
+            }
         } elseif ($firstOperator == "between") {
             $lowVal = $firstValue[0];
             $highVal = $firstValue[1];
-            $query = $query->where($ft." BETWEEN ".$lowVal." AND ".$highVal);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." BETWEEN ".$lowVal." AND ".$highVal);
+            } else {
+                $query = $query->andWhere($ft." BETWEEN ".$lowVal." AND ".$highVal);
+            }
         } elseif ($firstOperator == "not_between") {
             $lowVal = $firstValue[0];
             $highVal = $firstValue[1];
-            $query = $query->where($ft." NOT BETWEEN ".$lowVal." AND ".$highVal);
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." NOT BETWEEN ".$lowVal." AND ".$highVal);
+            } else {
+                $query = $query->andWhere($ft." NOT BETWEEN ".$lowVal." AND ".$highVal);
+            }
         } elseif ($firstOperator == "ends_with") {
-            $query = $query->where($ft." LIKE"." '%".strtouppser($firstValue)."'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." LIKE"." '%".strtoupper($firstValue)."'");
+            } else {
+                $query = $query->andWhere($ft." LIKE"." '%".strtoupper($firstValue)."'");
+            }
         } elseif ($firstOperator == "not_ends_with") {
-            $query = $query->where($ft." NOT LIKE"." '%".strtoupper($firstValue)."'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." NOT LIKE"." '%".strtoupper($firstValue)."'");
+            } else {
+                $query = $query->andWhere($ft." NOT LIKE"." '%".strtoupper($firstValue)."'");
+            }
         } elseif ($firstOperator == "contains") {
-            $query = $query->where($ft." LIKE"." '%".strtoupper($firstValue)."%'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." LIKE"." '%".strtoupper($firstValue)."%'");
+            } else {
+                $query = $query->andWhere($ft." LIKE"." '%".strtoupper($firstValue)."%'");
+            }
         } elseif ($firstOperator == "not_contains") {
-            $query = $query->where($ft." NOT LIKE"." '%".strtoupper($firstValue)."%'");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." NOT LIKE"." '%".strtoupper($firstValue)."%'");
+            } else {
+                $query = $query->andWhere($ft." NOT LIKE"." '%".strtoupper($firstValue)."%'");
+            }
         } elseif ($firstOperator == "is_empty") {
-            $query = $query->where($ft." ".$firstValue."= ''");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." ".$firstValue."= ''");
+            } else {
+                $query = $query->andWhere($ft." ".$firstValue."= ''");
+            }
         } elseif ($firstOperator == "is_not_empty") {
-            $query = $query->where($ft." ".$firstValue."!=''");
+            if ($condition == "OR") {
+                $query = $query->orWhere($ft." ".$firstValue."!=''");
+            } else {
+                $query = $query->andWhere($ft." ".$firstValue."!=''");
+            }
         } else {
             try {
                 throw new Exception('Impossible operation; try another operator');
