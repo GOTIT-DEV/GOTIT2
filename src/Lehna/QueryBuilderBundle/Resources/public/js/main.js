dee6dfabfec3af062cb7b0601b1784dcd38080eb
Elsa <elsa-marlene.mendes-dos-ramos-varanda@etu.univ-lyon1.fr>
2020-04-01 14:27:52 +0200

    frontend ready

--- src/Lehna/QueryBuilderBundle/Resources/public/js/main.js
+++ src/Lehna/QueryBuilderBundle/Resources/public/js/main.js
@@ -5,57 +5,61 @@
  * This software is distributed without any warranty.
  * 
  * Authors : Thierno Diallo, Maud Ferrer and Elsa Mendes. 
-*/ 
+*/
 
 
 
 // Initializing the first query block
 $(document).ready(function () {
-  
-  // Init menu for choosing the first table
-  let dropdown = $('#first-table');
-  dropdown.empty();
-  dropdown.append('<option selected="true" disabled>Choose table</option>');
-  dropdown.prop('selectedIndex', 0);
+
+
 
   // Filling the menu with all the tables in the database
-  $.getJSON("init", function (data) {
-    $.each(data, function (key, entry) {
+  $.getJSON("init", function (init_data) {
+    // Init menu for choosing the first table
+    let dropdown = $('#first-table');
+    dropdown.empty()
+      .append('<option selected="true" disabled>Choose table</option>')
+      .prop('selectedIndex', 0);
+
+    $.each(init_data, function (key, entry) {
       dropdown.append($('<option></option>').attr('value', entry.human_readable_name).text(entry.human_readable_name));
     })
-  });
 
-  // Choosing the fields you want to return and the constraints to apply
-  $('#first-table').click(function () {
-    
-    // Init list of fields
-    var showData1 = $('#show-data1');
-    var showData2 = $('#show-data2');
-    var liste = document.getElementById("first-table");
-    let target_table = $(liste).val();
+    //initialization of the query builder 
+    $('#initial-table-constraints').queryBuilder({
+      plugins: ['bt-tooltip-errors'],
+      filters: [
+        {
+          id: "empty",
+          label: "empty",
+          type: "integer"
+        }]
+    })
+    $('.reset').click(function () {
+      let target = $(this).data('target')
+      $(target).queryBuilder('reset')
+    })
 
-    // Init query-builder with fields and filters
-    $.getJSON('init', function (data) {
+    // what occurs when you choose a table and maybe change it 
+    $('#first-table').change(function (event) {
+      let target_table = event.target.value
+      let table_data = init_data[target_table];
 
-      let table_data = data[target_table];
-      $('.builder-basic').eq(0).queryBuilder('setFilters',true, table_data.filters);
-      
+      // Init query-builder with fields and filters
+      $('#initial-table-constraints').queryBuilder('setFilters', true, table_data.filters);
+
+      // Init list of fields
+      $("#first-table-selects").empty()
+
       var items = table_data.filters.map(function (item) {
-        return item.label;
-      });
+        return Mustache.render(
+          '<li><input type="checkbox" name="my_form" value="{{label}}" checked/><label>{{label}}</label></li>',
+          item
+        )
+      })
 
-      showData1.empty();
-      showData2.empty();
-  
-      if (items.length) {
-        
-        var content = '<input type="checkbox" name="my_form" value="' + items.join('"/><br><input type="checkbox" name="my_form" value="') + '"/><br>';
-        var content2= '<label>' + items.join('</label><br><label>') + '</label><br>';
-        var list = $('<ul style="padding-left: 0px;"/>').html(content);
-        var list2= $('<ul />').html(content2);
-        showData1.append(list);
-        showData2.append(list2);
-      }
+      $("#first-table-selects").html(items.join(''))
     });
   });
 });
@@ -67,7 +71,6 @@ joints = [
   'left join',
   'right join',
   'cross join',
-  'self join',
   'full join'
 ];
 
@@ -79,128 +82,119 @@ joints = [
  * - choice of adjacent table of the new chosen table
  * - possibility to choose specific fields to return and constraints to apply
  */
-function addJoint() {
-  var cont = $('.add-constraints').eq(-1);
-  var temp = document.getElementsByTagName("template")[0];
-  var clon = temp.content.cloneNode(true);
-  cont.append(clon);
+function addJoint(block_id) {
+  let newBlock = Mustache.render(
+    $("#form-block-template").html(),
+    { id: block_id }
+  )
+
+  $('#add-constraints').append(newBlock);
+
+  newBlock = $("#form-block-" + block_id)
+
+  // query builder initialization 
+  newBlock.find(".builder-basic").queryBuilder({
+    plugins: ['bt-tooltip-errors'],
+    filters: [{
+        id: "empty",
+        label: "empty",
+        type: "integer"
+    }]
+  })
+  //reset buttom
+  newBlock.find('.reset').click(function () {
+    let target = $(this).data('target')
+    $(target).queryBuilder('reset')
+  })
+
+  return newBlock
 }
 
 
+
+
+let new_block_id = 0
 // Choosing what joint to make between a table chosen previously and an adjacent table, the fields to return and the constraints to apply
 $('#add-joint').click(function () {
 
-  // Init menus for previously chosen tables and adjacent tables
-  let menu_tables = $('.previous-table:last > option');
-  let adj_table = $('.adjacent-tables').eq(-1).find(':selected').text();
+  $.getJSON('init', function (init_data) {
+    // add 1 at each click on add-joint
+    new_block_id += 1
 
-  // Adding a block of query
-  addJoint();
+    // Adding a block of query
+    let newBlock = addJoint(new_block_id)
 
-  // Filling the menus (Previously chosen tables, Joints and Adjacent tables)
-  let dropdown = $('.joints').eq(-1);
-  let dropdown2 = $('.previous-table').eq(-1);
-  dropdown.empty();
-  dropdown.append('<option selected="true" disabled>Choose joint</option>');
-  dropdown.prop('selectedIndex', 0);
-  dropdown2.empty();
- 
-  // Filling the menu of the previous tables for the first added block
-  if (menu_tables.length == 0) {
-    dropdown2.append('<option selected="true" disabled>Choose one previous table</option>');
-    dropdown2.prop('selectedIndex', 0);
-    let first_selection = $('#first-table').find(':selected').text(); 
-    dropdown2.append($('<option></option>').attr('value', first_selection).text(first_selection));
-  }
-  // Filling the menu of the previous tables for the second added block and after
-  else {
-    $.each(menu_tables, function (key, entry) {
-      dropdown2.append($('<option></option>').attr('value', entry.value).text(entry.value));
+    // Filling the menu containing the possible joints
+    let dropdown = newBlock
+      .find('.joints')
+      .empty()
+      .append('<option selected="true" disabled>Choose join</option>')
+      .prop('selectedIndex', 0);
+
+
+    $.each(joints, function (index, value) {
+      dropdown.append($('<option></option>').attr('value', value).text(value));
+
     });
-    dropdown2.append($('<option></option>').attr('value', adj_table).text(adj_table));
-  }
-
-  // Filling the menu containing the possible joints
-  $.each(joints, function (index, value) {
-    dropdown.append($('<option></option>').attr('value', value).text(value));
-
-  });
 
 
-  // Filling the menu containing the adjacent tables of the new chosen table
-  $('.previous-table').eq(-1).change(function () {
+  
+    //previous tables 
 
-    // Init the menu 
-    let dropdown = $('.adjacent-tables').eq(-1);
-    dropdown.empty();
-    dropdown.append('<option selected="true" disabled>Choose adjacent table</option>');
-    dropdown.prop('selectedIndex', 0);
+    let all_adj_tables = $(".adjacent-tables").map(function(){
+      return $(this).val()
+    }).get()
 
-    var texte = $('.previous-table').eq(-1).find(":selected").text();
+    let first_selection = $('#first-table').val()
 
-    // Filling the menu dynamically
-    $.getJSON("init", function (data) {
+    all_adj_tables.push(first_selection)
+    all_adj_tables = [... new Set(all_adj_tables)]
 
-      var texte2 = data[texte];
-      $.each(texte2.relations, function (key, value) {
+    let dropdown2 = newBlock.find('.previous-table').empty()
+        dropdown2.append('<option selected="true" disabled>Choose one previous table</option>')
+                  .prop('selectedIndex', 0)
+                  $.each(all_adj_tables, function (index, value) {
+                    dropdown2.append($('<option></option>').attr('value', value).text(value));
+              
+                  });
+
+
+    //when you select or change the value of the previous table you want to select 
+    newBlock.find(".previous-table").change(function (event) {
+      let target_table = event.target.value
+      // Init the menu 
+      let dropdown = newBlock
+        .find('.adjacent-tables')
+        .empty()
+        .append('<option selected="true" disabled>Choose adjacent table</option>')
+        .prop('selectedIndex', 0);
+
+      var table_data = init_data[target_table];
+      $.each(table_data.relations, function (key, value) {
         dropdown.append($('<option></option>').attr('value', key).text(key));
       });
     })
-  });
+
+    //when you click to select an adjacent table 
+    newBlock.find(".adjacent-tables").change(function (event) {
+      let target_table = event.target.value
+      let table_data = init_data[target_table];
 
 
-  // Choosing the fields you want to return and the constraints to apply for each new query
-  $('.new-constraints').click(function () {
+      // Init query-builder with fields and filters of the selected table 
+      newBlock.find('.builder-basic').queryBuilder('setFilters', true, table_data.filters);
 
-    if($(this).is(":checked")==false){
-      return;
-    }
-
-    // Init the fields of the chosen adjacent table
-    var showData3 = $('#show-data3').eq(-1);
-    var showData4= $('#show-data4').eq(-1);
-    var texte = $('.adjacent-tables').eq(-1).find(':selected').text();
-
-    // Init a block of query-builder with the menu of fields and the filters to apply 
-    $.getJSON('init', function (data) {
-      
-      $('.builder-basic').eq(-1).queryBuilder({
-        plugins: ['bt-tooltip-errors'],
-      
-        filters:  [ 
-          {
-          id: "empty",
-          label: "empty",
-          type: "integer"
-        }],
-      
-        // rules: rules_basic2
-      });
-
-      $('.builder-basic').eq(-1).queryBuilder('setFilters',true, data[texte].filters);
-
-      $('.reset').on('click', function() {
-        var target = $(this).data('target');
-      
-        $('.builder-'+target).eq(-1).queryBuilder('reset');
-      });
-
-
-      var items = data[texte].filters.map(function (item) {
-        return item.label;
-      });
-      showData3.empty();
-      showData4.empty();
-
-      if (items.length) {
-        var content = '<input type="checkbox" name="my_form2" value="' + items.join('"/><br><input type="checkbox" name="my_form2" value="') + '"/><br>';
-        var content2= '<label>' + items.join('</label><br><label>') + '</label><br>';
-        var list = $('<ul style="padding-left: 0px;"/>').html(content);
-        var list2= $('<ul />').html(content2);
-        showData3.append(list);
-        showData4.append(list2);
-      }
-    });
+      // Init list of fields
+      let selects_block = newBlock.find(".table-selects")
+      selects_block.empty()
+      var items = table_data.filters.map(function (item) {
+        return Mustache.render(
+          '<li><input type="checkbox" name="my_form2" value="{{label}}"/><label>{{label}}</label></li>',
+          item
+        )
+      })
+      selects_block.html(items.join(''))
+    })
   });
 });
 
@@ -217,74 +211,77 @@ function removeDiv() {
  * Read and convert the form's fields into json when SEARCH is clicked 
  */
 
-$.get("init", function(data){
+$.get("init", function (data) {
 
-  $("#submit-button").click(function(){
+  $("#submit-button").click(function () {
     let data_initial = get_form_initial()
     let data_join_blocks = get_form_block_data(data)
 
-    var jsonData = {"initial": data_initial,"joins": data_join_blocks};
+    var jsonData = { "initial": data_initial, "joins": data_join_blocks };
 
-    $.ajax({ 
-      url : 'query', //query_test
+    $.ajax({
+      url: 'query', //query_test
       type: 'POST',
       data: jsonData,
       dataType: 'json',
-      success : function(response){
+      success: function (response) {
         console.log(response);
       }
     });
-    })
-   // fin du callback associé au bouton Search
- })
+  })
+  // fin du callback associé au bouton Search
+})
 
 
-  function get_form_initial() {
-    
-    var table1 = document.getElementById("first-table");
-    var table = table1.options[table1.selectedIndex].value;
-    if($('#first-constraints').is(":checked")==true){
-      var constraintsTable1 = $('.builder-basic').eq(0).queryBuilder('getRules');}
-    else {var constraintsTable1=null};
-    var fields=[]
-    $('input:checked[name=my_form]').each(function() {
-      fields.push($(this).val());
-      });
-    
+function get_form_initial() {
 
-    return {table,constraintsTable1,fields}
-
+  var table1 = document.getElementById("first-table");
+  var table = table1.options[table1.selectedIndex].value;
+  if ($('#first-constraints').is(":checked") == true) {
+    var constraintsTable1 = $('.builder-basic').eq(0).queryBuilder('getRules');
   }
+  else { var constraintsTable1 = null };
+  var fields = []
+  $('input:checked[name=my_form]').each(function () {
+    fields.push($(this).val());
+  });
 
 
-  function get_form_block_data(init_data) {
+  return { table, constraintsTable1, fields }
 
-    let block_list = $(".formBlock")
-    let data = block_list.map(function () {
-      let block = $(this)
-      let adj_table = block.find("#adjacent-tables_id").val()
-      let formerT = block.find("#formerTable").val()
-      let idJoin = block.find("#joint_table").val()
-      
-    var relationAdj= init_data[formerT].relations[adj_table]
+}
+
+
+function get_form_block_data(init_data) {
+
+  let block_list = $(".formBlock")
+  let data = block_list.map(function () {
+    let block = $(this)
+    let adj_table = block.find("#adjacent-tables_id").val()
+    let formerT = block.find("#formerTable").val()
+    let idJoin = block.find("#joint_table").val()
+
+    var relationAdj = init_data[formerT].relations[adj_table]
 
     var sourceField = relationAdj.from
     var targetField = relationAdj.to
 
 
-      if($('#second_constraints').is(":checked")==true){
-        var constraintsTable2 = $('.builder-basic').eq(-1).queryBuilder('getRules');}
-      else {var constraintsTable2=null;};
+    if ($('#second_constraints').is(":checked") == true) {
+      var constraintsTable2 = $('.builder-basic').eq(-1).queryBuilder('getRules');
+    }
+    else { var constraintsTable2 = null; };
 
-      var fields=[]
-      $('input:checked[name=my_form2]').each(function() {
-        fields.push($(this).val());
-        });
+    var fields = []
+    $('input:checked[name=my_form2]').each(function () {
+      fields.push($(this).val());
+    });
 
 
-      return { 'formerTable' : formerT,'join' : idJoin, 'adjacent_table' : adj_table, 'sourceField': sourceField, 'targetField': targetField,  'constraints':constraintsTable2, 'fields':fields }}).toArray()
-      return data 
-  }
+    return { 'formerTable': formerT, 'join': idJoin, 'adjacent_table': adj_table, 'sourceField': sourceField, 'targetField': targetField, 'constraints': constraintsTable2, 'fields': fields }
+  }).toArray()
+  return data
+}
 
 
 
@@ -312,7 +309,7 @@ $(document).ready(_ => {
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 50px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -327,8 +324,5 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
-
 
 
