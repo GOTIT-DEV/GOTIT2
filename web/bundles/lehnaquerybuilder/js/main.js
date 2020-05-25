f3364da3c69b0730bf8f691ba818126db58859a9
Maud <maud.ferrer9@orange.fr>
2020-05-25 17:57:48 +0200

    Adding plugin for image zoom on modal (testing, not working properly)

--- web/bundles/lehnaquerybuilder/js/main.js
+++ web/bundles/lehnaquerybuilder/js/main.js
@@ -490,12 +490,14 @@ let myModal = document.getElementById("logigal-database-model");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 let dbImg = document.getElementById("logical-db-img");
+$("#logical-db-image").elevateZoom();
 let myModalImg = document.getElementById("db-img");
 let captionText = document.getElementById("caption");
 dbImg.onclick = function () {
   myModal.style.display = "block";
   myModalImg.src = this.src;
   captionText.innerHTML = this.alt;
+  $("#db-img").elevateZoom();
 };
 
 let closeImg = document.getElementsByClassName("close")[0];
