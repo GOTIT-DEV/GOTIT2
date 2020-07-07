e8eb9faac1e2d2b7ff3aa1b96945c90535c77b27
pgrison <philippe.grison@mnhn.fr>
2020-07-07 16:15:26 +0200

    enable to sort all column in the external sequence list of records

--- src/Bbees/E3sBundle/Controller/SequenceAssembleeExtController.php
+++ src/Bbees/E3sBundle/Controller/SequenceAssembleeExtController.php
@@ -56,7 +56,7 @@ class SequenceAssembleeExtController extends AbstractController
         ));
     }
 
-    /**
+        /**
      * Returns in json format a set of fields to display (tab_toshow) with the following criteria: 
      * a) 1 search criterion ($ request-> get ('searchPhrase')) insensitive to the case and  applied to a field
      * b) the number of lines to display ($ request-> get ('rowCount'))
@@ -65,6 +65,118 @@ class SequenceAssembleeExtController extends AbstractController
      * @Route("/indexjson", name="sequenceassembleeext_indexjson", methods={"POST"})
      */
     public function indexjsonAction(Request $request, GenericFunctionE3s $service, TranslatorInterface $translator)
+    {
+        // load Doctrine Manager          
+        $em = $this->getDoctrine()->getManager();
+        //
+        $rowCount = ($request->get('rowCount')  !== NULL) ? $request->get('rowCount') : 10; 
+        $orderBy = ($request->get('sort')  !== NULL) ? array_keys($request->get('sort'))[0]." ".array_values($request->get('sort'))[0] : "sq.date_of_update DESC, sq.id DESC";  
+        $minRecord = intval($request->get('current')-1)*$rowCount;
+        $maxRecord = $rowCount; 
+        // initializes the searchPhrase variable as appropriate and sets the condition according to the url idFk parameter
+        $where = 'LOWER(sq.external_sequence_code) LIKE :criteriaLower';
+        $searchPhrase = $request->get('searchPhrase');
+        if ( $request->get('searchPatern') !== null && $request->get('searchPatern') !== '' && $searchPhrase == '') {
+            $searchPhrase = $request->get('searchPatern');
+        }
+        if ( $request->get('idFk') !== null && $request->get('idFk') !== '') {
+            $where .= ' AND sq.sampling_fk = '.$request->get('idFk');
+        }
+        
+        // Search for the list to show
+        $tab_toshow =[];
+        $rawSql = "SELECT  sq.id, st.site_code, sampling.sample_code, country.country_name, municipality.municipality_code,
+        sq.external_sequence_creation_date, sq.date_of_creation, sq.date_of_update, voc_sq_identification_criterion.code as code_sq_identification_criterion,
+	sq.external_sequence_code, sq.external_sequence_alignment_code, rt_sq.taxon_name as last_taxname_sq, ei_sq.identification_date as last_date_identification_sq,
+        sq.external_sequence_primary_taxon, sq.external_sequence_specimen_number, sq.external_sequence_accession_number,
+        voc_gene.code as voc_external_sequence_gene_code, voc_status.code as voc_external_sequence_status_code, voc_date_precision.vocabulary_title as voc_date_precision_title,
+        sq.creation_user_name, user_cre.username as user_cre_username , user_maj.username as user_maj_username,
+        string_agg(DISTINCT source.source_title , ' ; ') as list_source, count(motu_number.id) motu
+	FROM external_sequence sq 
+                LEFT JOIN user_db user_cre ON user_cre.id = sq.creation_user_name
+                LEFT JOIN user_db user_maj ON user_maj.id = sq.update_user_name
+		JOIN sampling ON sampling.id = sq.sampling_fk
+			JOIN site st ON st.id = sampling.site_fk
+                        LEFT JOIN country ON st.country_fk = country.id
+                        LEFT JOIN municipality ON st.municipality_fk = municipality.id
+                LEFT JOIN external_sequence_is_published_in esip ON esip.external_sequence_fk = sq.id
+                    LEFT JOIN source ON esip.source_fk = source.id
+                LEFT JOIN vocabulary voc_gene ON sq.gene_voc_fk = voc_gene.id
+                LEFT JOIN vocabulary voc_status ON sq.external_sequence_status_voc_fk = voc_status.id
+                LEFT JOIN vocabulary voc_date_precision ON sq.date_precision_voc_fk = voc_date_precision.id
+                LEFT JOIN motu_number ON motu_number.external_sequence_fk = sq.id
+		LEFT JOIN identified_species ei_sq ON ei_sq.external_sequence_fk = sq.id
+			LEFT JOIN (SELECT MAX(ei_sqi.id) AS maxei_sqi 
+				FROM identified_species ei_sqi 
+				GROUP BY ei_sqi.external_sequence_fk) ei_sq2 ON (ei_sq.id = ei_sq2.maxei_sqi)
+			LEFT JOIN taxon rt_sq ON ei_sq.taxon_fk = rt_sq.id
+                        LEFT JOIN vocabulary voc_sq_identification_criterion ON ei_sq.identification_criterion_voc_fk = voc_sq_identification_criterion.id"
+        ." WHERE ".$where." 
+        GROUP BY sq.id, st.site_code, sampling.sample_code, country.country_name, municipality.municipality_code,
+        sq.external_sequence_creation_date, sq.date_of_creation, sq.date_of_update, voc_sq_identification_criterion.code, 
+	sq.external_sequence_code, sq.external_sequence_alignment_code, rt_sq.taxon_name, ei_sq.identification_date,
+        sq.external_sequence_primary_taxon, sq.external_sequence_specimen_number, sq.external_sequence_accession_number,
+        voc_gene.code, voc_status.code, voc_date_precision.vocabulary_title,
+        sq.creation_user_name, user_cre.username, user_maj.username" 
+        ." ORDER BY ".$orderBy;
+        // execute query and fill tab to show in the bootgrid list (see index.htm)
+        $stmt = $em->getConnection()->prepare($rawSql);
+        $stmt->bindValue('criteriaLower', strtolower($searchPhrase).'%');
+        $stmt->execute();
+        $entities_toshow = $stmt->fetchAll();
+        $nb = count($entities_toshow);
+        $entities_toshow = ($request->get('rowCount') > 0 ) ? array_slice($entities_toshow, $minRecord, $rowCount) : array_slice($entities_toshow, $minRecord);
+    
+        foreach($entities_toshow as $key => $val){
+             $tab_toshow[] = array("id" => $val['id'], "sq.id" => $val['id'],
+                "sq.external_sequence_alignment_code" => $val['external_sequence_alignment_code'],
+                "sq.external_sequence_code" => $val['external_sequence_code'],
+                "sq.external_sequence_accession_number" => $val['external_sequence_accession_number'],
+                "voc_gene.code" => $val['voc_external_sequence_gene_code'],
+                "voc_date_precision.vocabulary_title" => $val['voc_date_precision_title'],
+                "sq.external_sequence_creation_date" => $val['external_sequence_creation_date'],
+                "sq.external_sequence_primary_taxon" => $val['external_sequence_primary_taxon'],
+                "sq.external_sequence_specimen_number" => $val['external_sequence_specimen_number'],
+                "voc_status.code" => $val['voc_external_sequence_status_code'],
+                "sq.date_of_creation" => $val['date_of_creation'],
+                "sq.date_of_update" => $val['date_of_update'],
+                "sampling.sample_code" => $val['sample_code'],
+                "last_taxname_sq" => $val['last_taxname_sq'],
+                "last_date_identification_sq" => $val['last_date_identification_sq'],
+                "voc_sq_identification_criterion.code" => $val['code_sq_identification_criterion'],
+                "list_source" => $val['list_source'],
+                "motu" => $val['motu'],
+                "country.country_name" => $val['country_name'],
+                "municipality.municipality_code" => $val['municipality_code'],
+                "creation_user_name" => $val['creation_user_name'], "user_cre.username" => $val['user_cre_username'] ,"user_maj.username" => $val['user_maj_username']
+             );
+         }
+
+        // Ajax answer
+        $response = new Response ();
+        $response->setContent ( json_encode ( array (
+            "current"    => intval( $request->get('current') ), 
+            "rowCount"  => $rowCount,            
+            "rows"     => $tab_toshow, 
+            "searchPhrase" => $searchPhrase,
+            "total"    => $nb // total data array				
+            ) ) );
+        // If it is an Ajax request: returns the content in json format
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;          
+    } 
+
+    
+    /**
+     * Returns in json format a set of fields to display (tab_toshow) with the following criteria: 
+     * a) 1 search criterion ($ request-> get ('searchPhrase')) insensitive to the case and  applied to a field
+     * b) the number of lines to display ($ request-> get ('rowCount'))
+     * c) 1 sort criterion on a collone ($ request-> get ('sort'))
+     *
+     * @Route("/indexjson", name="sequenceassembleeext_indexjson_backup", methods={"POST"})
+     */
+    public function indexjsonAction_backup(Request $request, GenericFunctionE3s $service, TranslatorInterface $translator)
     {
         // load Doctrine Manager          
         $em = $this->getDoctrine()->getManager();
